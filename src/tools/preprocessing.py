4bf9763ece93adf01f94776dba380314d8557a7d
ZanZver <zan.zver98@gmail.com>
2021-05-03 20:00:29 +0100

    small fix

--- src/tools/preprocessing.py
+++ src/tools/preprocessing.py
@@ -95,51 +95,53 @@ def preprocess_images(
 
     # Check if folders exist
     for folder in os.listdir(dataset_path):
-        assert os.path.isdir(os.path.join(dataset_path, folder)) #if error shows here, remove .DS_store from data file, MacOS creates that hidden file
-        class_names.append(folder)
+        if (folder != ".DS_Store"):
+            assert os.path.isdir(os.path.join(dataset_path, folder)) #if error shows here, remove .DS_store from data file, MacOS creates that hidden file
+            class_names.append(folder)
 
     # For every folder in the dataset path
     for folder in os.listdir(dataset_path):
-        file_progress_bar = tqdm(os.listdir(
-            os.path.join(dataset_path, folder)))
+        if (folder != ".DS_Store"):
+            file_progress_bar = tqdm(os.listdir(
+                os.path.join(dataset_path, folder)))
 
-        # For every image in that folder
-        for filename in file_progress_bar:
+            # For every image in that folder
+            for filename in file_progress_bar:
 
-            # If file is image
-            if filename.lower().endswith("png") or filename.lower().endswith("jpg") or filename.lower().endswith("jpeg"):
+                # If file is image
+                if filename.lower().endswith("png") or filename.lower().endswith("jpg") or filename.lower().endswith("jpeg"):
 
-                # Read the grayscale image
-                gray_img = cv2.imread(os.path.join(dataset_path, folder, filename))
+                    # Read the grayscale image
+                    gray_img = cv2.imread(os.path.join(dataset_path, folder, filename))
 
-                # Convert that image to RGB
-                color_img = cv2.cvtColor(gray_img, cv2.COLOR_BGR2RGB)
+                    # Convert that image to RGB
+                    color_img = cv2.cvtColor(gray_img, cv2.COLOR_BGR2RGB)
 
-                # Resize to the required size
-                img = cv2.resize(color_img, (width, height))
+                    # Resize to the required size
+                    img = cv2.resize(color_img, (width, height))
 
-                # Convert image to array
-                img = tf.keras.preprocessing.image.img_to_array(img)
-                img = np.expand_dims(img, axis=0)
+                    # Convert image to array
+                    img = tf.keras.preprocessing.image.img_to_array(img)
+                    img = np.expand_dims(img, axis=0)
 
-                # Preprocessing for feature composer
-                if imagenet == True:
-                    if framework == "torch":
-                        img = tf.keras.applications.imagenet_utils.preprocess_input(img, mode="torch")
-                    else:
-                        img = tf.keras.applications.imagenet_utils.preprocess_input(img, mode="tf")
+                    # Preprocessing for feature composer
+                    if imagenet == True:
+                        if framework == "torch":
+                            img = tf.keras.applications.imagenet_utils.preprocess_input(img, mode="torch")
+                        else:
+                            img = tf.keras.applications.imagenet_utils.preprocess_input(img, mode="tf")
 
-                # Add the image array to the features list
-                features.append(img)
+                    # Add the image array to the features list
+                    features.append(img)
 
-                # Add the appropriate row from the identity matrix to the labels list
-                # E.g.: If the classes are: COVID, NORMAL and PNEUMONIA,
-                # the labels will be [1, 0, 0], [0, 1, 0] and [0, 0, 1].
-                labels.append(identity_matrix[class_names.index(folder)])
+                    # Add the appropriate row from the identity matrix to the labels list
+                    # E.g.: If the classes are: COVID, NORMAL and PNEUMONIA,
+                    # the labels will be [1, 0, 0], [0, 1, 0] and [0, 0, 1].
+                    labels.append(identity_matrix[class_names.index(folder)])
 
-                file_progress_bar.set_description(f"Loading images from directory {folder}")
-        
-        logger.info(f"Loading images from directory {folder}")
+                    file_progress_bar.set_description(f"Loading images from directory {folder}")
+            
+            logger.info(f"Loading images from directory {folder}")
 
     # Convert the features list to a vertical stack array.
     # This gets rid of any array of length 1.
