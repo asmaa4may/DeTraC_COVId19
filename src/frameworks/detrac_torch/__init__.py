0249e231a55c6b95a4142823bd4f5f8ec608b7da
Asmaa Abbas <34330067+asmaa4may@users.noreply.github.com>
2021-07-14 10:11:26 +0200

    Delete src directory

--- src/frameworks/detrac_torch/__init__.py
+++
@@ -1,3 +0,0 @@
-from .network import Net, set_device
-from .feature_extractor import train_feature_extractor
-from .feature_composer import train_feature_composer, infer
--- src/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from .construct_composed_dataset import execute_decomposition
-from .kfold import KFold_cross_validation_split
-from .multiclass_confusion_matrix import multiclass_confusion_matrix
-from .preprocessing import preprocess_single_image, preprocess_images
-from .extraction_and_metrics import extract_features, compute_confusion_matrix, compose_classes
